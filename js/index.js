b6b41161a61f867cd4a3ebe27f89537f086acdf1
NateTheDev1 <nthnlrichards@gmail.com>
2020-03-12 11:32:20 -0400

    3/10 Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const navLinks = document.querySelectorAll(".nav-link");
+
+//Adds a hover effect to each nav link
+navLinks.forEach(l => {
+  l.addEventListener("mouseover", e => {
+    l.style.color = "red";
+    setTimeout(() => {
+      l.style.color = "gray";
+    }, 500);
+  });
+});
+
+//Logs the key pressed
+window.addEventListener("keydown", e => {
+  alert(e.key);
+});
+
+//Change background on scroll with back and forth switch
+let switchNum = 0;
+window.addEventListener("wheel", e => {
+  if (switchNum === 0) {
+    document.body.style.background = "white";
+    switchNum = 1;
+  } else {
+    document.body.style.background = "blue";
+    switchNum = 0;
+  }
+});
